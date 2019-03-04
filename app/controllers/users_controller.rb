f6a817da7c1e442a0eed16064c6042475bb02cd6
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-04 18:48:15 -0500

    adds basic user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def new
+    @user = User.new
+    
+  end
+
+  def create
+    user = User.create(user_params)
+
+    redirect_to user_path(user)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :name, :age, :password_digest)
+  end
 end
