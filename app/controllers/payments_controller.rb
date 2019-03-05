cd98e82a1bc723a4f3a8df5fd83b6ea3feec43bf
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-05 18:38:33 -0500

    cleaned extraneous comments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -18,7 +18,9 @@ class PaymentsController < ApplicationController
     payment = Payment.new(payment_params)
     payment.debt_id = flash[:debt_id]
     debt = Debt.find(payment.debt_id)
+
     # debt is used for validations
+
     if debt.amount > 0 && payment.payment_amount <= debt.amount
       payment.user_id = current_user.id
       payment.make_payment(debt)
@@ -45,5 +47,3 @@ class PaymentsController < ApplicationController
     params.require(:payment).permit(:user_id, :debt_id, :payment_amount, :comment)
   end
 end
-
-# Payment(index, show, new, create, delete)
