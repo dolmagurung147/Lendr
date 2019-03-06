b07a4685f427f91296ad4fb4613f2c3974c4fbc9
github username <github email address>
2019-03-06 14:46:08 -0500

    removed total lock bug and added reimbursement

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ApplicationRecord
   has_secure_password
 
   def total
-    self.debts.inject(0) {|sum, debt| sum + debt.amount }
+    self.debts.inject(0) {|sum, debt| sum += debt.amount }
+  end
+
+  def all_payback_debts
+    self.debts.select do |debt|
+      debt.payback
+    end
+  end
+
+  def all_reimbursements
+    all_reimbursements = []
+    all_payback_debts.each do |debt|
+      debt.payments.each do |payment|
+        all_reimbursements << payment
+      end
+    end
+    all_reimbursements
   end
 end
